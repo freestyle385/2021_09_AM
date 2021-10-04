9779b4bdf6205e22d629756d42e1c54a91c2fad3
freestyle385 <freestyle385@gmail.com>
2021-10-04 11:26:12 +0900

    게시물 작성/수정/삭제/상세조회 시 권한 제한(로그인 필요)_1004

--- src/main/java/com/sbs/java/am/servlet/ArticleDeleteServlet.java
+++ src/main/java/com/sbs/java/am/servlet/ArticleDeleteServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.sbs.java.am.Config;
 import com.sbs.java.am.exception.SQLErrorException;
@@ -25,6 +26,17 @@ public class ArticleDeleteServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
+
+		// 세션 정보 받아오기 및 로그인 상태 확인
+		HttpSession session = request.getSession();
+		String sessionLoginId = (String) session.getAttribute("sessionLoginId");
+
+		if (sessionLoginId == null) {
+			response.getWriter().append(
+					String.format("<script> alert('로그인 후 이용해주세요.'); history.back(); </script>"));
+			// history.back() : 이전으로 돌아가기
+			return;
+		}
 
 		// 커넥터 드라이버 활성화
 		String driverName = Config.getDBDriverClassName();
--- src/main/java/com/sbs/java/am/servlet/ArticleWriteServlet.java
+++ src/main/java/com/sbs/java/am/servlet/ArticleWriteServlet.java
@@ -6,14 +6,29 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/article/write")
 public class ArticleWriteServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		// 세션 정보 받아오기 및 로그인 상태 확인
+		HttpSession session = request.getSession();
+		String sessionLoginId = (String) session.getAttribute("sessionLoginId");
+
+		if (sessionLoginId == null) {
+			response.getWriter().append(String.format("<script> alert('로그인 후 이용해주세요.'); history.back(); </script>"));
+			// history.back() : 이전으로 돌아가기
+			return;
+		}
+
 		request.getRequestDispatcher("/jsp/article/write.jsp").forward(request, response);
 	}
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
